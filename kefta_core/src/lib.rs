6e9e7123a8c75738b615fd639786e11044c0fe31
Sam Huddart <sam02h.huddart@gmail.com>
2023-10-11 11:51:00 +0100

    more prototyping

--- kefta_core/src/lib.rs
+++ kefta_core/src/lib.rs
@@ -1,80 +1,167 @@
 mod model;
-mod error;
-mod types;
+mod _syn;
+mod util;
 
-mod test {
-    use std::fmt::{Formatter, Write};
-    use crate::model::{FromMeta, MetaSource, MetaVisitor};
+mod _test {
+    use std::fmt::Formatter;
+    use proc_macro2::{Ident, Span, TokenStream};
+    use quote::quote;
+    use syn::{Attribute, Meta, parse2};
+    use syn::parse::{Parse, ParseBuffer, Parser, ParseStream};
+    use crate::model;
+    use crate::model::{FromMeta, MetaAccess, MetaError, MetaSource, MetaVisitor};
+    use crate::util::meta_match_visitor;
+
+    #[test]
+    fn test() {
+
+        let attr = quote!( example(foo, bar=10, baz(test)) );
+        let parse = Parser::parse2( Meta::parse, attr).unwrap();
+
+        Example::from_meta(parse).unwrap();
+    }
 
     struct Example {
         foo: bool,
-        bar: Option<char>
+        bar: usize,
+        baz: Vec<String>,
     }
 
     impl FromMeta for Example {
-        fn source<S>(source: &mut S) -> Result<Self, S::Error> where S: MetaSource {
-            struct _0 {
-                foo: Option<bool>,
-                bar: Option<Option<char>>
+        fn from_meta<S>(source: S) -> Result<Self, S::Error> where S: MetaSource {
+
+            #[derive(Debug)]
+            struct _Data {
+                example_foo: Option<bool>,
+                example_bar: Option<usize>,
+                example_baz: Option<Vec<String>>,
             }
 
-            impl _0 {
-                const INIT: Self = Self {
-                    foo: None,
-                    bar: None
-                };
-            }
+            struct _Visitor0<'a>(&'a mut _Data);
 
-            // this can be abstracted behind private util
-            impl MetaVisitor for _0 {
-                type Output = _0;
-
-                fn expecting(&self, fmt: &mut Formatter) -> std::fmt::Result {
-                    fmt.write_str("a list of attributes")
-                }
-
-                fn visit_list<S>(mut self, mut contents: S) -> Result<Self::Output, S::Error> where S: MetaSource {
-                    while contents.remaining() {
-                        contents.visit(_1(&mut self))?
-                    }
-                    Ok(self)
-                }
-            }
-
-            struct _1<'a>(&'a mut _0);
-
-            // actual meat and gravy
-            impl MetaVisitor for _1 {
+            impl<'a> MetaVisitor for _Visitor0<'a> {
                 type Output = ();
 
                 fn expecting(&self, fmt: &mut Formatter) -> std::fmt::Result {
-                    fmt.write_str("path: `foo` or `bar`")
+                    fmt.write_str("a meta identifier")
                 }
 
-                fn visit_path<S>(self, path: &str, source: &mut S) -> Result<Self::Output, S::Error> where S: MetaSource {
-                    // match path
+                fn visit_path<S>(self, source: S, path: Option<&str>, _span: Option<Span>) -> Result<Self::Output, S::Error> where S: MetaSource {
                     match path {
-                        "foo" if self.0.foo.is_some() => return Err(S::Error::duplicate("foo").with_span(source.pos())),
-                        "foo" => { self.0.foo = Some(bool::from_meta(source)?); },
+                        Some("example") => source.visit(_Visitor1(self.0)),
+                        _ => Ok(())
+                    }
+                }
 
-                        "bar" if self.0.bar.is_some() => return Err(S::Error::duplicate("foo").with_span(source.pos())),
-                        "bar" => { self.0.bar = Some(<Option<char>>::from_meta(source)?); },
+                fn visit_list<A>(self, mut access: A, span: Option<Span>) -> Result<Self::Output, A::Error> where A: MetaAccess {
+                    while access.remaining() {
+                        access.visit(Self(self.0))?;
+                    }
+                    Ok(())
+                }
+            }
 
-                        _ => return Err(S::Error::unknown("foo or bar", path).with_span(source.pos()))
+
+            struct _Visitor1<'a>(&'a mut _Data);
+
+            impl<'a> MetaVisitor for _Visitor1<'a> {
+                type Output = ();
+
+                fn expecting(&self, fmt: &mut Formatter) -> std::fmt::Result {
+                    fmt.write_str("a list of elements")
+                }
+
+                fn visit_list<A>(self, mut access: A, _span: Option<Span>) -> Result<Self::Output, A::Error> where A: MetaAccess {
+                    while access.remaining() {
+                        access.visit(_Visitor2(self.0))?;
+                    }
+                    Ok(())
+                }
+            }
+
+            struct _Visitor2<'a>(&'a mut _Data);
+
+            impl<'a> MetaVisitor for _Visitor2<'a> {
+                type Output = ();
+
+                fn expecting(&self, fmt: &mut Formatter) -> std::fmt::Result {
+                    fmt.write_str("a list of elements")
+                }
+
+                fn visit_path<S>(self, source: S, path: Option<&str>, span: Option<Span>) -> Result<Self::Output, S::Error> where S: MetaSource {
+                    match path {
+                        Some("foo") => { self.0.example_foo = Some(<bool as FromMeta>::from_meta(source)?) },
+                        _ => ()
                     };
                     Ok(())
                 }
             }
 
-            // call
-            let a = source.visit(_0::INIT)?;
+            let mut _data = _Data {
+                example_foo: None,
+                example_bar: None,
+                example_baz: None,
+            };
 
-            // extact
+            source.visit(_Visitor0(&mut _data))?;
+
+            println!("{:?}", _data);
+
             Ok(Self {
-                foo: a.foo.unwrap_or_default(),
-                bar: a.bar.unwrap_or_default(),
+                foo: match _data.example_foo {
+                    Some(x) => x,
+                    None => return Err(S::Error::custom("invalid", None)),
+                },
+                bar: 0,
+                baz: vec![],
             })
         }
+    }
 
+    struct Example2 {
+        foo: bool,
+        bar: usize,
+        baz: Vec<String>,
+    };
+
+    impl FromMeta for Example2 {
+        fn from_meta<S>(source: S) -> Result<Self, S::Error> where S: MetaSource {
+            struct _Def {
+                example_foo: Option<bool>,
+                example_bar: Option<usize>,
+                example_baz: Option<Vec<String>>
+            }
+
+            let mut _def = _Def {
+                example_foo: None,
+                example_bar: None,
+                example_baz: None,
+            };
+
+            meta_match_visitor! {
+                _Vis0 ( _Def ) {
+                    "example" => _Vis1
+                }
+            };
+
+            meta_match_visitor! {
+                _Vis1 ( _Def ) {
+                    "example" => {  }
+                }
+            };
+
+
+            source.visit(_Vis0(&mut _def))?;
+
+            Ok(Self {
+                foo: _def.example_foo
+                    .unwrap_or_else(|| Default::default()),
+                bar: _def.example_bar
+                    .unwrap_or_else(|| 0),
+                baz: _def.example_baz
+                    .unwrap_or_else(|| Vec::new()),
+            })
+        }
     }
 }
+
--- kefta_core/src/model.rs
+++ kefta_core/src/model.rs
@@ -1,47 +1,145 @@
 use std::fmt;
-use proc_macro2::{Span, TokenStream};
-use crate::error::{Error, ErrorDisplay};
+use std::iter::Peekable;
+use proc_macro2::{ Span, TokenStream};
 
-pub trait FromMeta {
-    fn from_meta<S>(source: &mut S) -> Result<Self, S::Error> where S: MetaSource;
+
+pub trait FromMeta: Sized {
+    fn from_meta<S>(source: S) -> Result<Self, S::Error> where S: MetaSource;
 }
 
-#[allow(unused_variables)]
 pub trait MetaVisitor: Sized {
     type Output;
 
-    fn expecting(&self, fmt: &mut fmt::Formatter) -> fmt::Result;
+    fn expecting(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result;
 
-    fn visit_path<S>(self, path: &str, source: &mut S) -> Result<Self::Output, S::Error> where S: MetaSource {
-        Err(Error::expected(&self, "path"))
+    fn visit_path<S>(self, source: S, path: Option<&str>, span: Option<Span>) -> Result<Self::Output, S::Error>
+        where S: MetaSource
+    {
+        let _ = source;
+
+        Err(S::Error::expected(
+            &self,
+            format!("path ({})", path.unwrap_or("::")).as_str(),
+            span
+        ))
     }
 
-    fn visit_list<S>(self, contents: S) -> Result<Self::Output, S::Error> where S: MetaSource {
-        Err(Error::expected(&self, "list"))
+    fn visit_word<E>(self, span: Option<Span>) -> Result<Self::Output, E>
+        where E: MetaError
+    {
+        Err(E::expected(
+            &self,
+            "word",
+            span
+        ))
     }
 
-    fn visit_marker<E>(self) -> Result<Self::Output, E> where E: Error {
-        Err(Error::expected(&self, "marker"))
+    fn visit_value<E>(self, tokens: TokenStream, span: Option<Span>) -> Result<Self::Output, E>
+        where E: MetaError
+    {
+        Err(E::expected(
+            &self,
+            format!("value ({})", tokens).as_str(),
+            span
+        ))
     }
 
-    fn visit_value<E>(self, stream: TokenStream) -> Result<Self::Output, E> where E: Error {
-        Err(Error::expected(&self, "stream"))
+    fn visit_list<A>(self, access: A, span: Option<Span>) -> Result<Self::Output, A::Error>
+        where A: MetaAccess
+    {
+        Err(A::Error::expected(
+            &self,
+            "list of meta",
+            None
+        ))
     }
 }
 
 pub trait MetaSource {
-    type Error: Error;
+    type Error: MetaError;
 
-    fn remaining(&self) -> bool;
+    fn visit<V: MetaVisitor>(self, visitor: V) -> Result<V::Output, Self::Error>;
+}
 
-    fn position(&self) -> Span;
+pub trait MetaAccess {
+    type Error: MetaError;
 
-    fn visit<V>(&mut self, visit: V) -> Result<V::Output, Self::Error> where V: MetaVisitor;
+    fn remaining(&mut self) -> bool;
+
+    fn visit<V: MetaVisitor>(&mut self, visitor: V) -> Result<V::Output, Self::Error>;
 }
 
 
-impl<'a, T> ErrorDisplay for T where T: MetaVisitor {
-    fn description(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+impl<T> MetaSource for Vec<T> where T: MetaSource {
+    type Error = T::Error;
+
+    fn visit<V: MetaVisitor>(self, visitor: V) -> Result<V::Output, Self::Error> {
+        struct _Access<U>(Peekable<<Vec<U> as IntoIterator>::IntoIter>);
+
+        impl<U> MetaAccess for _Access<U> where U: MetaSource {
+            type Error = U::Error;
+
+            fn remaining(&mut self) -> bool {
+                self.0.peek().is_some()
+            }
+
+            fn visit<V: MetaVisitor>(&mut self, visitor: V) -> Result<V::Output, Self::Error> {
+                if let Some(x) = self.0.next() {
+                    x.visit(visitor)
+                } else {
+                    unreachable!()
+                }
+            }
+        }
+
+        visitor.visit_list(_Access(self.into_iter().peekable()), None)
+    }
+}
+
+pub trait MetaError: Sized {
+    fn custom(message: impl MetaErrorFmt, at: Option<Span>) -> Self;
+
+    fn expected(expected: impl MetaErrorFmt, found: impl MetaErrorFmt, at: Option<Span>) -> Self {
+        Self::custom(format!("expected `{}`, found `{}`", expected.as_fmt(), found.as_fmt()).as_str(), at)
+    }
+}
+
+pub struct MetaErrorFormatter<'a, T: MetaErrorFmt>(&'a T);
+
+impl<'a, T: MetaErrorFmt> fmt::Display for MetaErrorFormatter<'a, T> {
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.0.format(f)
+    }
+}
+
+impl<'a, T: MetaErrorFmt> MetaErrorFmt for MetaErrorFormatter<'a, T> {
+    #[inline]
+    fn format(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        self.0.format(fmt)
+    }
+
+}
+
+pub trait MetaErrorFmt {
+    fn format(&self, fmt: &mut fmt::Formatter) -> fmt::Result;
+
+    #[inline]
+    fn as_fmt(&self) -> MetaErrorFormatter<Self> where Self: Sized {
+        MetaErrorFormatter(self)
+    }
+}
+
+impl<'a> MetaErrorFmt for &'a str {
+    fn format(&self, fmt: &mut  fmt::Formatter) -> fmt::Result {
+        fmt.write_str(&self)
+    }
+}
+
+
+impl<T> MetaErrorFmt for &T where T: MetaVisitor {
+    #[inline]
+    fn format(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
         self.expecting(fmt)
     }
 }
